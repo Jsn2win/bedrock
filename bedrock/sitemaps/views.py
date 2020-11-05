ae06ee088e08c0a76ba25a2516c1ddda769153f0
Paul McLanahan <pmac@mozilla.com>
2020-11-05 17:32:32 -0500

    Convert sitemaps URLs to use views and data from db
    Push this commit to prod only after the first commit in this PR.
    
    Fix #9658

--- bedrock/sitemaps/views.py
+++ bedrock/sitemaps/views.py
@@ -1,1 +1,33 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+from bedrock.sitemaps.models import SitemapURL
+
+
+class SitemapView(TemplateView):
+    content_type = 'text/xml'
+
+    def _get_locale(self):
+        if self.kwargs['is_none']:
+            locale = ''
+        else:
+            # can come back as empty string
+            # should be None here if not a real locale
+            locale = getattr(self.request, 'locale', None) or None
+
+        return locale
+
+    def get_template_names(self):
+        if self._get_locale() is None:
+            return ['sitemap_index.xml']
+        else:
+            return ['sitemap.xml']
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        locale = self._get_locale()
+        if locale is None:
+            ctx['locales'] = SitemapURL.objects.all_locales()
+        else:
+            ctx['paths'] = SitemapURL.objects.all_for_locale(locale)
+
+        return ctx
